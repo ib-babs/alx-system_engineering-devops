33a2ceae840f3837754eb4348962f1fac6081a6c
ib-babs <babatundeibrahim426@gmail.com>
2023-11-27 07:51:35 +0100

    All commit

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    // pid_t pid;
+    // if (pid == -1)
+    // {
+    //     perror("fork");
+    //     exit(-1);
+    // }
+    // else
+    // {
+    //     for (size_t i = 0; i < 5; i++)
+    //     {
+    //         pid = getpid();
+    //         printf("Zombie process created, PID: %d\n", pid);
+    //     }
+    // }
+}
