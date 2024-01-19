dca1c300292fea9d2f907bd35d9fe7eae4f2def5
ib-babs <babatundeibrahim426@gmail.com>
2024-01-19 21:07:29 +0100

    Edition of 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-# install_a_package.pp 'flask' using pip3
+# install_a_package.pp
+package {'python3-pip':
+ensure => installed
+}
 
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'python3-pip'
+exec {'1-install_a_package':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    path    => '/usr/bin/',
+    require => Package['python3-pip'],
 }
