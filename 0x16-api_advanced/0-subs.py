145a9b355b31baeb395c3122bb5c6c9335a96921
ib-babs <babatundeibrahim426@gmail.com>
2024-03-05 14:55:30 +0100

    PEP8 conformity

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Get the number of the REDDIT subscribers"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
