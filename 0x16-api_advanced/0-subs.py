9b0dbc99cebce045986a59c1d51d5bcfa9d29360
ib-babs <babatundeibrahim426@gmail.com>
2024-03-05 17:04:44 +0100

    PEP8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""Reddit API - Application Programming Interface
-Queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers)
-for a given subreddit."""
+"""
+Contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Get the number of the REDDIT subscribers"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {
-        "User-Agent": '0x16-api_advanced:project:\
-v1.0.0 (by /u/drkhalil05)'
-    }
-    subscribers = requests.get(url, headers=headers, allow_redirects=False)
-
-    if subscribers.status_code == 200:
-        return (subscribers.json()['data']['subscribers'])
-    else:
-        return (0)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/drkhalil005)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
