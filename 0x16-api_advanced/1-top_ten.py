c03299302c74e95fc4b4bdf32047f0135156c1d3
ib-babs <babatundeibrahim426@gmail.com>
2024-03-05 15:26:34 +0100

    Top ten hot post

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 
 def top_ten(subreddit):
     """Get the top 10 hot posts"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {
         "User-Agent": '0x16-api_advanced:project:\
 v1.0.0 (by /u/drkhalil05)'
@@ -16,7 +16,7 @@ v1.0.0 (by /u/drkhalil05)'
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.jsons()
+        data = response.json()
         titles = [r['data']['title'] for r in data['data']['children']]
         for r in titles:
             print(r)
