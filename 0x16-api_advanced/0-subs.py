5da0796fd0ff13797d6113af4686330ee156a417
ib-babs <babatundeibrahim426@gmail.com>
2024-03-05 14:45:58 +0100

    PEP8 conformity

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,9 +11,13 @@ import requests
 def number_of_subscribers(subreddit):
     """Get the number of the REDDIT subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    subscribers = requests.get(url, headers={
-        'User-Agent': 'Mozilla'
-    }, allow_redirects=False)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+                Chrome/96.0.4664.110 Safari/537.36"
+    }
+
+    subscribers = requests.get(url, headers=headers, allow_redirects=False)
 
     if subscribers.status_code == 200:
         json_result = subscribers.json()
