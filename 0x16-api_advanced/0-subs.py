88b5c6754fe3f598e9bc8551502900008bb7f081
ib-babs <babatundeibrahim426@gmail.com>
2024-03-06 21:01:34 +0100

    New commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,17 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
+
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/drkhalil05)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+v1.0.0 (by /u/drkhalil05)'})
+    if r.status_code == 200:
+        try:
+            sub = r.json()
+            result = sub.get('data').get('subscribers')
+            if result is not None:
+                return (result)
+        except Exception as e:
+            pass
+    else:
+        return 0
